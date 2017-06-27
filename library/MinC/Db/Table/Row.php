4ea8f2d0c56e1e275c94b92c9d34406d76ef4fa0
Gabriel Avaloni <avaloni@users.noreply.github.com>
2017-06-27 16:05:08 -0300

    [FIX] Adapta o get das colunas das tabelas para funcionar com o padrão de nomenclatura do PGSQL

--- library/MinC/Db/Table/Row.php
+++ library/MinC/Db/Table/Row.php
@@ -8,5 +8,11 @@
  */
 class MinC_Db_Table_Row extends Zend_Db_Table_Row
 {
-
+    public function __get($columnName){
+        try{
+            return parent::__get($columnName);
+        }catch(Zend_Db_Table_Row_Exception $ex){
+            return parent::__get(strtolower($columnName));
+        }
+    }
 }
