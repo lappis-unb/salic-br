698cc8e45b6874175421506712d0d81cb1fc89b8
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-08-14 12:29:45 -0300

    [UPDATE] Adicionando configurações para utilização do XDebug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,6 +24,7 @@ if ! [ -d "/var/www/salic-br/application" ]; then
 
     ls -la /var/www/salic-br
 
+
     echo "[ ****************** ] Downloading composer "
 
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -38,5 +39,42 @@ if ! [ -d "/var/www/salic-br/application" ]; then
 
     echo "Complete! The application has been successfully copied to /var/www/salic-br"
 fi
+
+# X-Debug
+
+echo "$XDEBUG_REMOTE_ENABLE"
+
+if ! [ -v $XDEBUG_REMOTE_ENABLE ] ; then
+    echo "[ ****************** ] Starting install of XDebug and dependencies."
+		pecl shell-test xdebug && echo "Package xdebug Installed" || (
+			yes | pecl install xdebug
+			echo "zend_extension="`find /usr/local/lib/php/extensions/ -iname 'xdebug.so'` > $XDEBUGINI_PATH
+		    	echo "xdebug.remote_enable=$XDEBUG_REMOTE_ENABLE" >> $XDEBUGINI_PATH
+
+		    	if ! [ -v $XDEBUG_REMOTE_AUTOSTART ] ; then
+		        	echo "xdebug.remote_autostart=$XDEBUG_REMOTE_AUTOSTART" >> $XDEBUGINI_PATH
+		    	fi
+		    	if ! [ -v $XDEBUG_REMOTE_CONNECT_BACK ] ; then
+		        	echo "xdebug.remote_connect_back=$XDEBUG_REMOTE_CONNECT_BACK" >> $XDEBUGINI_PATH
+		    	fi
+	    		if ! [ -v $XDEBUG_REMOTE_HANDLER ] ; then
+		        	echo "xdebug.remote_handler=$XDEBUG_REMOTE_HANDLER" >> $XDEBUGINI_PATH
+		    	fi
+	    		if ! [ -v $XDEBUG_PROFILER_ENABLE ] ; then
+		        	echo "xdebug.profiler_enable=$XDEBUG_PROFILER_ENABLE" >> $XDEBUGINI_PATH
+		    	fi
+		    	if ! [ -v $XDEBUG_PROFILER_OUTPUT_DIR ] ; then
+	        		echo "xdebug.profiler_output_dir=$XDEBUG_PROFILER_OUTPUT_DIR" >> $XDEBUGINI_PATH
+		    	fi
+		    	if ! [ -v $XDEBUG_REMOTE_PORT ] ; then
+	        		echo "xdebug.remote_port=$XDEBUG_REMOTE_PORT" >> $XDEBUGINI_PATH
+		    	fi
+
+		    	echo "xdebug.remote_host="`/sbin/ip route|awk '/default/ { print $3 }'` >> $XDEBUGINI_PATH
+			echo "[ ****************** ] Ending install of XDebug and dependencies."
+
+	)
+fi
+
 echo "[ ****************** ] Ending Endpoint of Application"
 exec "$@"
