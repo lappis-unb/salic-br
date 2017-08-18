13f94bea6bd2ff40f67c38e98a92e0eef8255193
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-08-18 15:47:55 -0300

    [UPDATE] Correção de case e adição de nome da tabela

--- application/modules/agente/models/DbTable/Verificacao.php
+++ application/modules/agente/models/DbTable/Verificacao.php
@@ -1,27 +1,13 @@
 <?php
 
-/**
- * Class Agente_Model_DbTable_Verificacao
- *
- * @name Agente_Model_DbTable_Verificacao
- * @package Modules/Agente
- * @subpackage Models/DbTable
- * @version $Id$
- *
- * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
- * @since 01/09/2016
- *
- * @copyright © 2012 - Ministerio da Cultura - Todos os direitos reservados.
- * @link http://salic.cultura.gov.br
- */
 class Agente_Model_DbTable_Verificacao extends MinC_Db_Table_Abstract
 {
-    protected $_banco = 'agentes';
+    protected $_primary = 'idVerificacao';
     protected $_schema = 'agentes';
-    protected $_name = 'verificacao';
+    protected $_name = 'Verificacao';
 
     const PROPOSTA_PARA_ANALISE_INICIAL = 96;
-    const PROPOSTA_EM_CONFORMIDADE_VISUAL_OU_AN&aacute;LISE_DOCUMENTAL = 97;
+    const PROPOSTA_EM_CONFORMIDADE_VISUAL_OU_ANALISE_DOCUMENTAL = 97;
     const PROPOSTA_EM_ANALISE_FINAL = 128;
 
     function combosNatureza($idTipo)
