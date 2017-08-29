2ef486268200774d5602b96fcc8d7f92801be221
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-08-28 21:29:51 -0300

    [UPDATE] Adição de tratativa criada pelo Gabriel para chamadas de campos que utilizam zend_db

--- library/MinC/Db/Table/Row.php
+++ library/MinC/Db/Table/Row.php
@@ -1,12 +1,12 @@
 <?php
 
-/**
- * @author: Wouerner <wouerner@gmail.com>
- * @author: Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
- * @author: Jorge Luiz Junior <juninhoecb@gmail.com>
- * @since: 17/08/16 19:53
- */
 class MinC_Db_Table_Row extends Zend_Db_Table_Row
 {
-
+    public function __get($columnName){
+        try{
+            return parent::__get($columnName);
+        }catch(Zend_Db_Table_Row_Exception $ex){
+            return parent::__get(strtolower($columnName));
+        }
+    }
 }
