85aa8b86208e6e95fe10a0ed11f03873e587ae82
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-09-05 16:36:31 -0300

    [UPDATE] Ajuste de case para consulta por CEP.

--- application/modules/default/models/DAO/Cep.php
+++ application/modules/default/models/DAO/Cep.php
@@ -3,60 +3,29 @@
 class Cep extends MinC_Db_Table_Abstract
 {
 
-//    protected $_schema = 'bddne.scdne';
-    protected $_schema = 'bddne.scdne';
-    protected $_name = 'vw_endereco';
+    protected $_schema = 'bddne.scDNE';
+    protected $_name = 'VW_ENDERECO';
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function init(){
+    public function init()
+    {
         parent::init();
     }
 
     /**
-     * Buscar o cep no banco de dados
-     * @access public
-     * @static
-     * @param integer $cep
-     * @return string $retorno
-     */
-    public static function buscarCepDB($cep)
-    {
-        $sql = "SELECT CEP,
-            logradouro,
-            tipo_logradouro,
-            bairro,
-            cidade,
-            uf,
-            idCidadeMunicipios,
-            dsCidadeMunicipios,
-            idCidadeUF,
-            DSCIDADEMUNICIPIOS AS dsCidadeUF
-            FROM BDDNE.scDNE.VW_ENDERECO
-            WHERE CEP = '$cep'";
-
-        $db= Zend_Db_Table::getDefaultAdapter();
-        $db->setFetchMode(Zend_DB::FETCH_ASSOC);
-
-        return $db->fetchRow($sql);
-    }
-
-    /**
-     *
      * @name buscarCEP
      * @param $cep
      * @return null|Zend_Db_Table_Row_Abstract
-     *
-     * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
-     * @since  ${DATE}
      *
      * @todo verificar por que o zend nao reconhece uma view para o mapeamento.
      */
     public function buscarCEP($cep)
     {
-        $db= Zend_Db_Table::getDefaultAdapter();
+        $db = Zend_Db_Table::getDefaultAdapter();
         $db->setFetchMode(Zend_DB::FETCH_ASSOC);
 
         $cols = array(
