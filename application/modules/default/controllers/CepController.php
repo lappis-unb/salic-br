d7e84655e4c9aef71e42bce24a9f07cf766beb8d
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-09-14 18:51:39 -0300

    [UPDATE] Ajuste no carregamento do CEP via ajax.

--- application/modules/default/controllers/CepController.php
+++ application/modules/default/controllers/CepController.php
@@ -1,22 +1,10 @@
 <?php
-/**
- * CepController
- * @author Equipe RUP - Politec
- * @author wouerner <wouerner@gmail.com>
- * @since 29/03/2010
- * @link http://www.cultura.gov.br
- */
-
 class CepController extends MinC_Controller_Action_Abstract
 {
-    /**
-     * Metodo para buscar o endereuo de acordo com o cep informado
-     * @access public
-     * @param void
-     * @return void
-     */
     public function cepAction()
     {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
 
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
         $strCharset = $config->resources->db->params->charset;
@@ -24,43 +12,26 @@ class CepController extends MinC_Controller_Action_Abstract
         header('Content-type: text/html; charset=' . $strCharset);
         $this->_helper->layout->disableLayout();
 
-        // recebe o cep sem mascara vindo via ajax
         $get = Zend_Registry::get('get');
         $cep = Mascara::delMaskCEP(Seguranca::tratarVarAjaxUFT8($get->cep));
 
         $cepObj = new Cep();
-        $resultado = $cepObj->buscarCEP($cep);
+        $resultadoBuscaCep = $cepObj->buscarCEP($cep);
 
-        if ($resultado) // caso encontre o cep
-        {
-            $_end         = $resultado['logradouro'];
-            $_complemento = $resultado['tipo_logradouro'];
-            $_bairro      = $resultado['bairro'];
-            $_uf          = $resultado['uf'];
-
-            // atribuica da cidade
-            if (empty($resultado['idcidademunicipios']) || empty($resultado['dscidademunicipios']))
-            {
-                // caso a cidade nao exista na tabela de municipios (tabela associada aos agentes)
-                // pega a primeira cidade do estado
-                $_cod_cidade = $resultado['idcidadeuf'];
-                $_cidade     = $resultado['dscidadeuf'];
+        $resultado = [];
+        if ($resultadoBuscaCep) {
+            $resultado['endereco'] = $resultadoBuscaCep['logradouro'];
+            $resultado['complemento'] = $resultadoBuscaCep['tipo_logradouro'];
+            $resultado['bairro'] = $resultadoBuscaCep['bairro'];
+            $resultado['uf'] = $resultadoBuscaCep['uf'];
+            $resultado['cod_cidade'] = $resultadoBuscaCep['idcidademunicipios'];
+            $resultado['cidade'] = $resultadoBuscaCep['dscidademunicipios'];
+            if (empty($resultadoBuscaCep['idcidademunicipios']) || empty($resultadoBuscaCep['dscidademunicipios'])) {
+                $resultado['cod_cidade'] = $resultadoBuscaCep['idcidadeuf'];
+                $resultado['cidade'] = $resultadoBuscaCep['dscidadeuf'];
             }
-            else
-            {
-                // caso a cidade exista na tabela de municipios (tabela associada aos agentes)
-                // pega a cidade da tabela de municipios
-                $_cod_cidade = $resultado['idcidademunicipios'];
-                $_cidade     = $resultado['dscidademunicipios'];
-            }
-
-            $buscarCEP = $_end . ":" . $_complemento . ":" . $_bairro . ":" . $_cod_cidade . ":" . $_cidade . ":" . $_uf . ";";
-        } // fecha if
-        else // caso nao ache o cep
-        {
-            $buscarCEP = "";
         }
-
-        $this->view->cep = $buscarCEP;
+        
+        $this->_helper->json($resultado);
     }
 }
